074108d72c703db8d94a173bda087d17171d2724
Mariy <family.home.9800@gmail.com>
2025-12-10 23:45:51 +0300

    uvm_spec.py

--- uvm_spec.py
+++ uvm_spec.py
@@ -1,18 +1,22 @@
 class UVMSpec:
-    # Коды операций для варианта 26
-    LOAD_CONST = 202    # 0xCA
-    READ_MEM = 156      # 0x9C
-    WRITE_MEM = 93      # 0x5D
-    SUB = 175           # 0xAF
+    """Спецификация УВМ для варианта 26"""
+    
+    # Коды операций
+    LOAD_CONST = 202    # 0xCA - Загрузка константы
+    READ_MEM = 156      # 0x9C - Чтение из памяти
+    WRITE_MEM = 93      # 0x5D - Запись в память
+    SUB = 175           # 0xAF - Вычитание
     
     @staticmethod
     def encode_load_const(constant):
-        """Кодирует команду LOAD константы"""
-        # A=202 (8 бит), B=constant (24 бита)
+        """
+        Кодирует команду LOAD константы
+        Формат: A=202 (8 бит), B=constant (24 бита)
+        Размер: 4 байта
+        """
         a = UVMSpec.LOAD_CONST
         b = constant & 0xFFFFFF  # Обеспечиваем 24 бита
         
-        # Упаковка в little-endian
         return [
             a & 0xFF,           # Байт 0: код операции
             (b >> 16) & 0xFF,   # Байт 1: старшие 8 бит константы
@@ -22,8 +26,11 @@ class UVMSpec:
     
     @staticmethod
     def encode_read_mem(offset):
-        """Кодирует команду READ из памяти"""
-        # A=156 (8 бит), B=offset (16 бит)
+        """
+        Кодирует команду READ из памяти
+        Формат: A=156 (8 бит), B=offset (16 бит)
+        Размер: 3 байта
+        """
         a = UVMSpec.READ_MEM
         b = offset & 0xFFFF  # Обеспечиваем 16 бит
         
@@ -35,8 +42,11 @@ class UVMSpec:
     
     @staticmethod
     def encode_write_mem(address):
-        """Кодирует команду WRITE в память"""
-        # A=93 (8 бит), B=address (24 бита)
+        """
+        Кодирует команду WRITE в память
+        Формат: A=93 (8 бит), B=address (24 бита)
+        Размер: 4 байта
+        """
         a = UVMSpec.WRITE_MEM
         b = address & 0xFFFFFF  # Обеспечиваем 24 бита
         
@@ -49,8 +59,11 @@ class UVMSpec:
     
     @staticmethod
     def encode_sub(address):
-        """Кодирует команду SUB (вычитание)"""
-        # A=175 (8 бит), B=address (24 бита)
+        """
+        Кодирует команду SUB (вычитание)
+        Формат: A=175 (8 бит), B=address (24 бита)
+        Размер: 4 байта
+        """
         a = UVMSpec.SUB
         b = address & 0xFFFFFF  # Обеспечиваем 24 бита
         
